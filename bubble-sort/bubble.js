b82990982ec9ab0f0a4d5bc9fde0786cf2bdc6c9
libing <libing@noah-fund.com>
2016-10-19 11:02:23 +0800

    add bubble sort

---
+++ bubble-sort/bubble.js
@@ -0,0 +1,20 @@
+/**
+ * Created by lib7311 on 2016/6/6.
+ */
+/**
+ * 使用传入的迭代器，对数组进行冒泡排序
+ * @param array
+ * @param iterator
+ * @param type '+'：升序; '-'：降序
+ */
+module.exports = function (array, iterator, type) {
+    var flag = true, //表示还处于无序
+        i = 1,
+        j = 1,
+        l = array.length;
+    for(; i < l && true; ++i){
+        for(; j < l; ){
+
+        }
+    }
+};

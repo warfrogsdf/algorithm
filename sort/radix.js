4c855abefa9ba7d9b219bd4d4eefbc7c2429a0ca
bing.li@aihuishou.com <bing.li@aihuishou.com>
2018-01-29 16:58:35 +0800

    type: feature profile: radix sort module: sort description:

--- sort/radix.js
+++ sort/radix.js
@@ -9,30 +9,76 @@ function swap(arr, i, j) {
   arr[j] = tmp;
 }
 
-export default function radix(arr, comp) {
+let bucket = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  []
+];
+
+bucket.reset = function () {
+  let i = 0;
+  for (; i < 20; ++i){
+    this[i] = [];
+  }
+};
+
+export default function radix(arr, comp, getNumber) {
   let len = arr.length,
     i,
     j,
+    index,
     tmp,
-    max = 0,
-    min = 0;
+    number,
+    positiveNum = 0,
+    negativeNum = 0;
+
   for (i = 0; i < len; ++i) {
     tmp = arr[i].toString().split('.');
-    if (tmp[0].length > max) {
-      max = tmp[0].length;
+    if (tmp[0] && tmp[0].length > positiveNum) {
+      positiveNum = tmp[0].length;
     }
-    if (tmp[1] && tmp[1].length > min) {
-      min = tmp[1].length;
+    if (tmp[1] && tmp[1].length > negativeNum) {
+      negativeNum = tmp[1].length;
     }
   }
+
   // 基数排序正数部分
-  for (i = 0; i < max; ++i) {
-    for (j = 0; j < len; ++j){
-      
+  for (i = 0; i < positiveNum; ++i) {
+    for (j = 0; j < len; ++j) {
+      // 一次获取i位上的数字，然后插入对象桶
+      number = getNumber(arr, j);
+      index = ~~(~~(~~number/Math.pow(10, i)) % 10);
+      if(number > 0){
+        bucket[index + 10].push(arr[j]);
+      }else{
+        bucket[9 + index].push(arr[j]);
+      }
     }
+    arr.length = 0;
+    arr = Array.prototype.concat.apply(arr, bucket);
+    bucket.reset();
   }
   // 基数排序负数部分
-  for (i = 0; i < min; ++i) {
+  for (i = 0; i < negativeNum; ++i) {
 
   }
+
+  return arr;
 };

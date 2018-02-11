0b6dd3a17748ff842db2c59d7764ba0725be8c1f
bing.li@aihuishou.com <bing.li@aihuishou.com>
2018-02-11 11:17:28 +0800

    type: feature profile: double array trie module: common description:

---
+++ data-structure/doubleArrayTrie.js
@@ -0,0 +1,11 @@
+function D() {
+
+}
+
+let dic = ['AC', 'ACE', 'ACFF', 'AD', 'CD', 'CF', 'ZQ'];
+
+export default {
+  initTrie: function () {
+
+  }
+}
---
+++ search/kmp.js
@@ -0,0 +1,18 @@
+/**
+ * [-1,-1,0,1,2,-1,0]，这里-1表示不存在，0表示存在长度为1，2表示存在长度为3。
+ * @param pStr
+ */
+function calNext(pStr) {
+  let len = pStr.length,
+    next = [-1],
+    i = 1,
+    j,
+    k;
+  for (; i < len; ++i) {
+
+  }
+}
+
+export default function kmp(sStr, pStr) {
+
+}
